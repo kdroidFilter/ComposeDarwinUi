f995f7dfe84577c1a9d505f30f4e2abd285eebc9
Elie Gambache <elyahou.hadass@gmail.com>
2026-02-04 17:42:56 +0200

    Refactor PlatformContextMenu for web: move implementation from jsMain to webMain, remove wasmJs variant, and replace stub with TODO placeholder.

--- darwinui/src/wasmJsMain/kotlin/io/github/kdroidfilter/darwinui/theme/PlatformContextMenu.wasmJs.kt
+++
@@ -1,6 +0,0 @@
-package io.github.kdroidfilter.darwinui.theme
-
-import androidx.compose.runtime.Composable
-
-@Composable
-internal actual fun PlatformContextMenuOverride(content: @Composable () -> Unit) = content()
--- darwinui/src/webMain/kotlin/io/github/kdroidfilter/darwinui/theme/PlatformContextMenu.web.kt
+++ darwinui/src/webMain/kotlin/io/github/kdroidfilter/darwinui/theme/PlatformContextMenu.web.kt
@@ -3,4 +3,6 @@ package io.github.kdroidfilter.darwinui.theme
 import androidx.compose.runtime.Composable
 
 @Composable
-internal actual fun PlatformContextMenuOverride(content: @Composable () -> Unit) = content()
+internal actual fun PlatformContextMenuOverride(content: @Composable (() -> Unit)) {
+    //TODO
+}
